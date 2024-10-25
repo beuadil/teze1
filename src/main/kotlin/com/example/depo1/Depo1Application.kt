eabd11ab87a611917e4a08e3715d7dce2489250d
beuadil <aqasimov3@std.beu.edu.az>
2024-10-25 10:40:37 +0400

    first attemp

---
+++ src/main/kotlin/com/example/depo1/Depo1Application.kt
@@ -0,0 +1,11 @@
+package com.example.depo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Depo1Application
+
+fun main(args: Array<String>) {
+    runApplication<Depo1Application>(*args)
+}
---
+++ src/test/kotlin/com/example/depo1/Depo1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.depo1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Depo1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
